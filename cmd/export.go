e4898abf0971e5277e2339ded16c4580b3b3abd4
Chaz Leong <13462818+cleong14@users.noreply.github.com>
2023-01-30 07:37:21 -1000

    add 'export s3' subcommand
    Signed-off-by: Chaz Leong <13462818+cleong14@users.noreply.github.com>

--- cmd/export.go
+++ cmd/export.go
@@ -4,22 +4,26 @@ import (
 	"bytes"
 	"context"
 	"fmt"
-	"github.com/gatecheckdev/gatecheck/pkg/artifact"
-	"github.com/gatecheckdev/gatecheck/pkg/export/defectdojo"
-	"github.com/spf13/cobra"
 	"os"
 	"time"
+
+	"github.com/spf13/cobra"
+
+	"github.com/gatecheckdev/gatecheck/pkg/artifact"
+	"github.com/gatecheckdev/gatecheck/pkg/export/defectdojo"
 )
 
 func NewExportCmd(service DDExportService, timeout time.Duration, engagement defectdojo.EngagementQuery) *cobra.Command {
-	var exportCmd = &cobra.Command{
+	// gatecheck export command
+	exportCmd := &cobra.Command{
 		Use:   "export",
 		Short: "Export a report to a target location",
 	}
 
-	var defectDojoCmd = &cobra.Command{
+	// gatecheck export defect-dojo command
+	defectDojoCmd := &cobra.Command{
 		Use:     "defect-dojo [FILE]",
-		Short:   "export raw scan report to Defect Dojo",
+		Short:   "Export raw scan report to DefectDojo",
 		Aliases: []string{"dd"},
 		Args:    cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -52,6 +56,44 @@ func NewExportCmd(service DDExportService, timeout time.Duration, engagement def
 			return service.Export(ctx, bytes.NewBuffer(fileBytes), engagement, ddScanType)
 		},
 	}
+
+	// gatecheck export s3 command
+	s3Cmd := &cobra.Command{
+		Use:     "s3 [FILE]",
+		Short:   "Export raw scan report to AWS S3",
+		Aliases: []string{"aws"},
+		Args:    cobra.ExactArgs(1),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// Open the file
+			f, err := os.Open(args[0])
+			if err != nil {
+				return fmt.Errorf("%w: %v", ErrorFileAccess, err)
+			}
+
+			ctx, cancel := context.WithTimeout(context.Background(), timeout)
+			defer cancel()
+
+			rType, err := artifact.InspectWithContext(ctx, f)
+			if err != nil {
+				return fmt.Errorf("%w: %v", ErrorEncoding, err)
+			}
+
+			var ddScanType defectdojo.ScanType
+			switch rType {
+			case artifact.Grype:
+				ddScanType = defectdojo.Grype
+			case artifact.Semgrep:
+				ddScanType = defectdojo.Semgrep
+			case artifact.Gitleaks:
+				ddScanType = defectdojo.Gitleaks
+			default:
+				return fmt.Errorf("%w: Unsupported file type", ErrorEncoding)
+			}
+
+			return service.Export(ctx, f, engagement, ddScanType)
+		},
+	}
 	exportCmd.AddCommand(defectDojoCmd)
+	exportCmd.AddCommand(s3Cmd)
 	return exportCmd
 }
