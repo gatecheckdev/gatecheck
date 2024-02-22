74cb231bd8570fa812f0982c0437f57c7431bb23
Bacchus Jackson <34331871+BacchusJackson@users.noreply.github.com>
2024-02-21 18:54:11 -0600

    feat: add v1 list command

---
+++ cmd/v1/list.go
@@ -0,0 +1,55 @@
+package cmd
+
+import (
+	"io"
+	"log/slog"
+	"os"
+
+	"github.com/gatecheckdev/gatecheck/pkg/gatecheck"
+	"github.com/spf13/cobra"
+	"golang.org/x/exp/slices"
+)
+
+var supportedTypes = []string{"grype", "semgrep", "gitleaks", "syft", "cyclonedx", "bundle"}
+
+func newListCommand() *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "print a summarized view of a security report artifact",
+		RunE:  runList,
+		Args:  cobra.MaximumNArgs(1),
+	}
+
+	cmd.Flags().StringP("input-type", "i", "", "the input filetype if using STDIN [grype|semgrep|gitleaks|syft]")
+
+	return cmd
+}
+
+func runList(cmd *cobra.Command, args []string) error {
+	slog.Debug("list artifact summary")
+	filename := ""
+
+	if len(args) > 0 {
+		filename = args[0]
+	}
+
+	inputType, _ := cmd.Flags().GetString("input-type")
+
+	var src io.Reader
+
+	switch {
+	case filename != "":
+		f, err := os.Open(filename)
+		if err != nil {
+			return err
+		}
+		src = f
+	case slices.Contains(supportedTypes, inputType):
+		filename = "stdin:" + inputType
+		src = cmd.InOrStdin()
+	default:
+		slog.Error("")
+	}
+
+	return gatecheck.List(cmd.OutOrStdout(), src, filename)
+}
---
+++ pkg/gatecheck/list.go
@@ -0,0 +1,68 @@
+package gatecheck
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"io"
+	"strings"
+
+	"log/slog"
+
+	"github.com/gatecheckdev/gatecheck/pkg/archive"
+	"github.com/gatecheckdev/gatecheck/pkg/artifacts/cyclonedx"
+	"github.com/gatecheckdev/gatecheck/pkg/artifacts/gitleaks"
+	"github.com/gatecheckdev/gatecheck/pkg/artifacts/grype"
+	"github.com/gatecheckdev/gatecheck/pkg/artifacts/semgrep"
+)
+
+func List(dst io.Writer, src io.Reader, inputFilename string) error {
+	var decoder interface {
+		Decode(any) error
+	}
+	var report interface {
+		String() string
+	}
+
+	decoder = json.NewDecoder(src)
+
+	switch {
+	case strings.Contains(inputFilename, "grype"):
+		slog.Debug("list", "filename", inputFilename, "filetype", "grype")
+		report = &grype.ScanReport{}
+
+	case strings.Contains(inputFilename, "semgrep"):
+		slog.Debug("list", "filename", inputFilename, "filetype", "semgrep")
+		report = &semgrep.ScanReport{}
+
+	case strings.Contains(inputFilename, "gitleaks"):
+		slog.Debug("list", "filename", inputFilename, "filetype", "gitleaks")
+		report = &gitleaks.ScanReport{}
+
+	case strings.Contains(inputFilename, "syft"):
+		slog.Debug("list", "filename", inputFilename, "filetype", "syft")
+		slog.Warn("syft decoder is not supported yet")
+		return nil
+
+	case strings.Contains(inputFilename, "cyclonedx"):
+		slog.Debug("list", "filename", inputFilename, "filetype", "cyclonedx")
+		report = &cyclonedx.ScanReport{}
+
+	case strings.Contains(inputFilename, "bundle"):
+		slog.Debug("list", "filename", inputFilename, "filetype", "bundle")
+		slog.Warn("gatecheck bundle decoder is not supported yet")
+		return nil
+
+	default:
+		slog.Error("invalid input filetype", "filename", inputFilename)
+		return errors.New("failed to list artifact content")
+	}
+
+	if err := decoder.Decode(report); err != nil {
+		slog.Error("list decode", "error", err)
+		return errors.New("failed to decode report")
+	}
+
+	_, err := fmt.Fprintln(dst, report.String())
+	return err
+}
